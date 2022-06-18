e9e16ed5ba2da31660433d088e1c08d547815172
Vyacheslav <v.guber2000@gmail.com>
2022-06-18 15:33:16 +0300

    Create ApiService, Repository, RetrofitInstance

--- app/src/main/java/com/vgubert/retrofitmoney/data/api/ApiService.kt
+++ app/src/main/java/com/vgubert/retrofitmoney/data/api/ApiService.kt
@@ -1,4 +1,15 @@
 package com.vgubert.retrofitmoney.data.api
 
+import com.vgubert.retrofitmoney.model.beznal.BeznalItem
+import com.vgubert.retrofitmoney.model.nal.NalichkaItem
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface ApiService {
+
+    @GET("p24api/pubinfo?json&exchange&coursid=5")
+    suspend fun getNalMoneys(): Response<NalichkaItem>
+
+    @GET("p24api/pubinfo?exchange&json&coursid=11")
+    suspend fun getBeznalMoneys(): Response<BeznalItem>
 }
--- app/src/main/java/com/vgubert/retrofitmoney/data/repository/Repository.kt
+++ app/src/main/java/com/vgubert/retrofitmoney/data/repository/Repository.kt
@@ -1,4 +1,16 @@
 package com.vgubert.retrofitmoney.data.repository
 
+import com.vgubert.retrofitmoney.data.api.RetrofitInstance
+import com.vgubert.retrofitmoney.model.beznal.BeznalItem
+import com.vgubert.retrofitmoney.model.nal.NalichkaItem
+import retrofit2.Response
+
 class Repository {
+    suspend fun getNal(): Response<NalichkaItem> {
+        return RetrofitInstance.api.getNalMoneys()
+    }
+
+    suspend fun getBeznal(): Response<BeznalItem> {
+        return RetrofitInstance.api.getBeznalMoneys()
+    }
 }
